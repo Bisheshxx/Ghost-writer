e2df0121f673eaf67ea28a7544508facdbdf32c6
Bisheshxx <bishesh.tuladhar1@gmail.com>
2026-04-16 23:03:08 +1200

    feat: added Project model

---
+++ src/models/projects.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+import { IProject } from "../types/project.types";
+import {
+  monthYearValidator,
+  nullableMonthYearValidator,
+} from "../utils/schema.validator";
+
+const ProjectSchema = new Schema<IProject>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    index: true,
+  },
+  projectTitle: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  details: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  startDate: {
+    type: String,
+    default: null,
+    validate: monthYearValidator,
+  },
+  endDate: {
+    type: String,
+    default: null,
+    validate: nullableMonthYearValidator,
+  },
+});
+
+export const Project = mongoose.model<IProject>("Project", ProjectSchema);
---
+++ src/types/project.types.ts
@@ -0,0 +1,9 @@
+import { Document, Types } from "mongoose";
+
+export interface IProject extends Document {
+  user: Types.ObjectId;
+  projectTitle: string;
+  startDate?: string;
+  endDate?: string;
+  details: string;
+}
